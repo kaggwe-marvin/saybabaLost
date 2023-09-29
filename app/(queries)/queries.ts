4eea508d16a9dc345fdf97f8cd3f7e6deb4c78d3
kaggwe-marvin <kiwanukamarvin597@gmail.com>
2023-09-30 00:43:37 +0300

    discarded unused imports and components

--- app/(queries)/queries.ts
+++ app/(queries)/queries.ts
@@ -1,33 +1,8 @@
 // queries.ts
 import { db } from "@/firebase/firebase";
-import {
-  collection,
-  query,
-  where,
-  orderBy,
-  limit,
-  getDocs,
-  startAt,
-  endAt,
-  GeoPoint,
-  updateDoc,
-  arrayUnion,
-  doc,
-  arrayRemove,
-  writeBatch,
-  collectionGroup,
-  endBefore,
-  startAfter,
-  getDoc,
-  onSnapshot,
-  Timestamp,
-  limitToLast,
-} from "firebase/firestore";
+import { collection, query, limit, getDocs } from "firebase/firestore";
 
 interface basicQueryProps {
-  limitCount: number;
-}
-interface realTimeQueryProps {
   limitCount: number;
 }
 
@@ -40,33 +15,6 @@ export const basicQuery = async ({ limitCount }: basicQueryProps) => {
     return querySnapshot.docs;
   } catch (error) {
     console.error("Error fetching data:", error);
-    throw error;
-  }
-};
-export const realTimeQuery = ({ limitCount }: realTimeQueryProps) => {
-  try {
-    const dataRef = collection(db, "lost");
-    const q = query(dataRef, limit(limitCount)); // Assuming you have a timestamp field
-
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      const results = querySnapshot.docs.map((doc) => {
-        const { id, name, collection, category, region } = doc.data();
-        return {
-          id,
-          name,
-          collection,
-          category,
-          region,
-        };
-      });
-
-      // Now you can use the 'results' array, which contains the limited and ordered real-time data.
-    });
-
-    // Remember to return the unsubscribe function if you want to stop listening to updates.
-    return unsubscribe;
-  } catch (error) {
-    console.error("Error fetching real-time data:", error);
     throw error;
   }
 };
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,34 +1,5 @@
 "use client";
-import { db } from "@/firebase/firebase";
-import { useState, useEffect } from "react";
-import { where, collection, query, getDocs } from "firebase/firestore";
-import { lostData } from "@/typings";
 function Search() {
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [filteredData, setFilteredData] = useState<lostData[]>([]);
-
-  useEffect(() => {
-    // Function to fetch data based on the searchQuery
-    const fetchData = async () => {
-      try {
-        const dataRef = collection(db, "lost"); // Replace with your Firestore collection name
-        const q = query(dataRef, where("region", "==", searchQuery)); // Replace 'fieldToSearch' with the actual field name you want to search
-
-        const querySnapshot = await getDocs(q);
-        const results: lostData[] = [];
-
-        querySnapshot.forEach((doc) => {
-          results.push({ id: doc.id, ...doc.data() } as lostData);
-        });
-
-        setFilteredData(results);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData(); // Call the function whenever searchQuery changes
-  }, [searchQuery]);
   return (
     <div className="join">
       <div>
