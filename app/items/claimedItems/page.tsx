d7b80f8a592084ed115f5b06c27225568981c989
kaggwe-marvin <kiwanukamarvin597@gmail.com>
2023-10-04 12:21:57 +0300

    add see more page

---
+++ app/items/claimedItems/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const claimed = () => {
+  return (
+    <>
+      <div>claimed</div>
+      <h1>under construction</h1>
+    </>
+  );
+};
+
+export default claimed;
---
+++ app/items/lostItems/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const lost = () => {
+  return (
+    <>
+      <div>claimed</div>
+      <h1>under construction</h1>
+    </>
+  );
+};
+
+export default lost;
