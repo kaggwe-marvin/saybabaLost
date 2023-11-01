c50b7d36a028d15eb506e5b7403b79c0a3215cc1
kaggwe-marvin <kiwanukamarvin597@gmail.com>
2023-11-01 16:53:18 +0300

    try tabs

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { whereQuery } from "../(queries)/queries";
 import ItemCard from "@/components/ItemCard";
-
+import Tabs from "@/components/Tabs";
 export default async function page() {
   const national = await whereQuery("category", "national");
   return (
     <main className="container mx-auto p-4">
-      <h1 className="text-5xl font-bold mb-2">U+2642;BaBaFinder!!</h1>
+      <h1 className="text-5xl font-bold mb-2">BaBaFinder!!</h1>
       <h2 className="text-2xl text-gray-700 mb-4">
         Find lost items or report found items.
       </h2>
+      <Tabs
+        tabIds={["National", "School", "Other"]}
+        renderTitle={(id) => `Tab ${id.toUpperCase()}`}
+        renderContent={(id) => `Tab content ${id.toUpperCase()}`}
+      />
       <ItemCard />
     </main>
   );
---
+++ components/Tabs.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode, useState } from "react";
+
+interface Props {
+  tabIds: string[];
+  renderTitle: (id: string) => ReactNode;
+  renderContent: (id: string) => ReactNode;
+}
+
+export default function Tabs({ tabIds, renderTitle, renderContent }: Props) {
+  const [selectedId, setSelectedId] = useState(tabIds[0]);
+
+  return (
+    <div>
+      {tabIds.map((id) => (
+        <button
+          key={id}
+          onClick={() => {
+            setSelectedId(id);
+          }}
+          style={{
+            border: "none",
+            background: id === selectedId ? "dodgerblue" : "transparent",
+          }}>
+          {renderTitle(id)}
+        </button>
+      ))}
+      <hr />
+      <div>{renderContent(selectedId)}</div>
+    </div>
+  );
+}
