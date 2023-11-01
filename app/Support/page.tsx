2f774ffb4388e5fe6a32cab90c67774f2c8c5fde
kaggwe-marvin <kiwanukamarvin597@gmail.com>
2023-11-01 14:05:12 +0300

    continuous

--- app/Support/page.tsx
+++ app/Support/page.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { whereQuery } from "../(queries)/queries";
+import ItemCard from "@/components/ItemCard";
 
-export const page = () => {
+export default async function page() {
+  const national = await whereQuery("category", "national");
   return (
-    <main>
-      <h1>Support Under Construction</h1>
+    <main className="container mx-auto p-4">
+      <h1 className="text-5xl font-bold mb-2">U+2642;BaBaFinder!!</h1>
+      <h2 className="text-2xl text-gray-700 mb-4">
+        Find lost items or report found items.
+      </h2>
+      <ItemCard />
     </main>
   );
-};
+}
